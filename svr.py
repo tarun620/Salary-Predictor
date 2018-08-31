2292ee50a879ab505f077112fa1fc8ea108877c9
tgsgkgvg <31451654+tgsgkgvg@users.noreply.github.com>
2018-08-31 11:46:14 +0530

    Add files via upload
    This machine learning model predicts the salary of the employee based on the years of experience they have and the level on which they were in their previous company.This helps the company to know how much salary to give to which employee.

---
+++ svr.py
@@ -0,0 +1,50 @@
+# Regression Template
+
+# Importing the libraries
+import os
+os.chdir("c:\\users\\lappy\\desktop")    
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+Y = dataset.iloc[:, 2:3].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X = sc_X.fit_transform(X)
+sc_Y = StandardScaler()
+Y = sc_Y.fit_transform(Y)
+
+# Fitting the Regression Model to the dataset
+# Create your regressor here
+from sklearn.svm import SVR
+regressor=SVR(kernel='rbf')
+regressor.fit(X,Y)
+# Predicting a new result
+Y_pred = sc_Y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
+
+# Visualising the Regression results
+plt.scatter(X, Y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, Y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show() 
